50a5f4e30ed7e87f80f9137170652e1f36c7654e
SotyoKey <104093465+SotyoKey@users.noreply.github.com>
2022-09-29 14:46:46 +0900

    Update post_blog.sh

--- scripts/post_blog.sh
+++ scripts/post_blog.sh
@@ -2,7 +2,7 @@
 set -eu
 
 for file_path in "$@"; do
-  if [[ ${file_path} =~ .*\.md ]] ; then
+  if [ [ ${file_path} =~ .*\.md ] ] ; then
     echo $file_path
     bin/blogsync push $file_path
   fi
