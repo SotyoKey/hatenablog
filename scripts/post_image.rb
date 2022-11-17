b611e1186f8e2f6653161b98b2343a57e1722dde
SotyoKey <104093465+SotyoKey@users.noreply.github.com>
2022-11-17 15:51:24 +0900

    Update post_image.rb

--- scripts/post_image.rb
+++ scripts/post_image.rb
@@ -68,6 +68,7 @@ class Executor
       hash = XmlSimple.xml_in(xml)
       puts "xmlhash: #{hash}"
       syntax = "[#{hash['syntax'].first}]"
+      syntax.gsub!(/image/, "plane")
 
       json[image_filename] = {
         syntax: syntax,
